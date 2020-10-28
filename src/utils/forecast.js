f0c9d7f4570ad0391a78d2fb88c6bfa07df7fcab
Jonas56 <younescasio02@gmail.com>
2020-10-28 15:50:48 +0100

    Update the forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined);
         } else if (body.error) {
-            callback('Location not found. Please try again!', undefined);
+            callback('Unable to find the location. Please try again!', undefined);
         } else {
             callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + '° out.' + ' There\'s ' + body.currently.precipProbability + '% chance to rain.')
         }
